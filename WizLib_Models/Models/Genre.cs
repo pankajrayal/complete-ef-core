3e73065c0cf4356e44a88a8d496ea076abe12906
Pankaj Rayal <https://github.dxc.com/>
2020-11-16 17:16:46 +0530

    Renaming Entities Columns/Properties

--- WizLib_Models/Models/Genre.cs
+++ WizLib_Models/Models/Genre.cs
@@ -3,6 +3,7 @@
     public class Genre
     {
         public int GenreId { get; set; }
-        public string Name { get; set; }
+        public string GenreName { get; set; }
+        public int DisplayOrder { get; set; }
     }
 }
