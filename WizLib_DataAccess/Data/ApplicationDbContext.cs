18667ee7e6aa1101feff87e96a0cbd246e227d40
Pankaj Rayal <https://github.dxc.com/>
2020-11-16 16:25:40 +0530

    Setup DbContext

---
+++ WizLib_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WizLib_DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+
+    }
+}
